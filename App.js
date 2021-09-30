61ef2d2e5754baa9f5e1fa53a31b419637863d6a
keshi2021 <84689938+keshi2021@users.noreply.github.com>
2021-09-29 17:42:46 -0700

    added some styling

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Habteab</Text>
-      <StatusBar style="auto" />
+      <View style={styles.red}>
+        <Text style={{ color: "blue" }}>Home</Text>
+        <Text style={{ color: "blue" }}>About</Text>
+        <Text style={{ color: "blue" }}>Contact</Text>
+      </View>
+      <View style={styles.gray}>
+        <Text style={{ color: "blue", fontSize: "42px" }}>Habteab Firezgi</Text>
+      </View>
+      <View style={styles.blue}>
+        <Text style={{ color: "white", fontSize: "22px" }}>This is Habteab's site </Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    flexDirection: "column",
+  },
+  red: {
+    flex: 1,
+    backgroundColor: "brown",
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  gray: {
+    flex: 8,
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  blue: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
